c4727094b4cf6621e8b6855a0f1068a03db6823a
Gregory.Johnson <3531op@gmail.com>
2021-03-30 14:42:59 -0400

    Initial commit. Starting with header.

---
+++ src/MainRouter.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class MainRouter extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <nav class='navbar navbar-expand-md navbar-dark fixed-top bg-dark'>
+            <div class='container-fluid'></div>
+          </nav>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default MainRouter;
